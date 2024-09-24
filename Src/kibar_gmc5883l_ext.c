06b729c9e77c8cd1178d5ac1aa061d8c3128a5dc
Alexander Kibarov <117196657+KibarovCmd@users.noreply.github.com>
2024-09-24 15:51:39 +0300

    Update kibar_gmc5883l_ext.c

--- Src/kibar_gmc5883l_ext.c
+++ Src/kibar_gmc5883l_ext.c
@@ -9,20 +9,20 @@
 
 KIBAR_STATUS KIBAR_I2C_WRITE_VALUE_ONE_BIT_TO_REGISTER(I2C_HandleTypeDef* kibar_i2c, uint8_t shift_value, uint8_t device_address, uint8_t register_address) {
 	uint8_t register_data;
-	HAL_StatusTypeDef statusone = HAL_I2C_Mem_Read(kibar_i2c, device_address, register_data, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
+	HAL_StatusTypeDef statusone = HAL_I2C_Mem_Read(kibar_i2c, device_address, register_address, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
 	if(statusone != HAL_OK) return kibar_ERROR;
 	register_data = register_data | (0x01 << shift_value);
-	statusone = HAL_I2C_Mem_Write(kibar_i2c, device_address, register_data, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
+	statusone = HAL_I2C_Mem_Write(kibar_i2c, device_address, register_address, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
 	if(statusone != HAL_OK) return kibar_ERROR;
 	else return kibar_OK;
 }
 
 KIBAR_STATUS KIBAR_I2C_WRITE_VALUE_ZERO_BIT_TO_REGISTER(I2C_HandleTypeDef* kibar_i2c, uint8_t shift_value, uint8_t device_address, uint8_t register_address) {
 	uint8_t register_data;
-	HAL_StatusTypeDef statusone = HAL_I2C_Mem_Read(kibar_i2c, device_address, register_data, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
+	HAL_StatusTypeDef statusone = HAL_I2C_Mem_Read(kibar_i2c, device_address, register_address, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
 	if(statusone != HAL_OK) return kibar_ERROR;
 	register_data = register_data & (~(0x01) << shift_value);
-	statusone = HAL_I2C_Mem_Write(kibar_i2c, device_address, register_data, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
+	statusone = HAL_I2C_Mem_Write(kibar_i2c, device_address, register_address, KIBAR_REGISTER_GENERAL_SIZE_, &register_data, KIBAR_REGISTER_GENERAL_SIZE_, KIBAR_TIMEOUT_);
 	if(statusone != HAL_OK) return kibar_ERROR;
 	else return kibar_OK;
 }
